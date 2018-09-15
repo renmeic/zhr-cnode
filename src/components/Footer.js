226e053719bcf67c2941d8bdfd5e2ce5da0695e5
zhr <renmeic@163.com>
2018-09-15 11:05:25 +0800

    finish footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 export default class Footer extends React.Component {
-	render() {
-		return (
-			<h1>footer</h1>
-		)
-	}
+  render() {
+    return (
+      <footer className="footer">
+        <div className="footer-wrap">
+          <p><a href="https://github.com/renmeic/zhr-cnode">源码地址</a></p>
+          <ul className="meta">
+            <li>
+              <span>CNode社区提供API</span>
+              <a href="https://cnodejs.org">
+                <img src="https://cnodejs.org/public/images/cnodejs.svg" alt="cnode"/>
+              </a>
+            </li>
+            <li>
+              <span>github-pages提供网站托管</span>
+              <a href="https://github.com">
+                <img src="./images/github.svg" alt="github"/>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    )
+  }
 }
