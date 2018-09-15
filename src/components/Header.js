3300dcd53e4a167358f1d1fe8b48c0a23fcd4e88
zhr <renmeic@163.com>
2018-09-15 11:17:56 +0800

    finish header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,18 @@ import React from 'react'
 export default class Header extends React.Component {
 	render() {
 		return (
-			<h1>header</h1>
+			<header className='header'>
+        <div className='header-wrap'>
+          <h1 className='logo'>
+            <a><img src='./images/cnodejs.svg' alt='cnodejs logo'/></a>
+          </h1>
+          <nav className='nav'>
+            <a>首页</a>
+            <a>关于作者</a>
+            <a>登录</a>
+          </nav>
+        </div>
+      </header>
 		)
 	}
 }
